facd88fea4516264093a678c74c008a969b30640
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-01-11 14:41:42 +0100

    Update dart_overview.dart

---
+++ dart_overview.dart
@@ -0,0 +1,40 @@
+// default introduction
+
+import 'dart:math';
+
+void main() async {
+  print('Compute pie using the Monte Carlo method.');
+  await for (final estimate in computePi().take(100)) {
+    print('3.147 $estimate');
+  }
+}
+
+Stream<double> computePi({int batch = 100000}) async* {
+  var total = 0;
+  var count = 0;
+  while (true) {
+    final points = generateRandom().take(batch);
+    final inside = points.where((p) => p.isInsideUnitCircle);
+
+    total += batch;
+    count += inside.length;
+    final ratio = count / total;
+
+    yield ratio * 4;
+  }
+}
+
+Iterable<Point> generateRandom([int? seed]) sync* {
+  final random = Random(seed);
+  while (true) {
+    yield Point(random.nextDouble(), random.nextDouble());
+  }
+}
+
+class Point {
+  final double x;
+  final double y;
+
+  const Point(this.x, this.y);
+  bool get IsInsideUnitCircle => x * x + y * y <= 1;
+}
