0c854880aa040001f404268751d6d00b2762499a
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-01-16 19:43:29 +0100

    Updating tour with basic dart language

---
+++ dart_offical_web/language/tour.dart
@@ -0,0 +1,9 @@
+// A basic dart program - define a function
+void printInteger(int aNumber){
+  print('The number is $aNumber.'); 
+  
+}
+void main() {
+  var number = 42; 
+  printInteger(number); 
+}
