17d156a97c7a9c9852d6a616b69624027a574718
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-01-16 20:00:07 +0100

    Pulling out from folder

--- dart_offical_web/language/tour.dart
+++
@@ -1,9 +0,0 @@
-// A basic dart program - define a function
-void printInteger(int aNumber){
-  print('The number is $aNumber.'); 
-  
-}
-void main() {
-  var number = 42; 
-  printInteger(number); 
-}
