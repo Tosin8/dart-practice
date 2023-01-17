0eaa9db72d915247579318dfd63a803dc9f113d3
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-01-17 20:05:59 +0100

    Updating with Strings

--- tour.dart
+++ tour.dart
@@ -9,4 +9,10 @@ void main() {
 }
 
 var name = 'Bob'; // variable
-final secondName = 'Bob'; // final variable 
+final secondName = 'Bob'; // final variable
+
+num x =
+    1; // x can have both int and double values, num declares the numbers as variable.
+
+double z =
+    1; // integer literals are automatically converted to doubles when necessary. 
