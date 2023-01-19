c09a85edb2bcc19fa04577d5c3e8d552265e38c0
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-01-19 11:53:35 +0100

    Updating with Operators

--- test1.dart
+++ test1.dart
@@ -1,16 +1,18 @@
 // testing functions for equality.
 
-void foo() {}
+void foo() {} // a top-level function
 
 class A {
-  static void bar() {}
-  void baz() {}
+  static void bar() {} // a static method
+  void baz() {} // an instance method
 }
 
 void main() {
   Function x;
+  // comparing top-level functions.
   x = foo;
   assert(foo == x);
+  // comparing instance methods.
   x = A.bar;
   assert(A.bar == x);
   var v = A();
@@ -18,5 +20,6 @@ void main() {
   var y = w;
   x = w.baz;
   assert(y.baz == x);
+  // these closures refer to different instances, so they're unequal.
   assert(v.baz != w.baz);
 }
