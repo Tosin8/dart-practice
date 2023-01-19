1d8dd2f3d4e025429767a68ede046292671cb285
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-01-19 11:48:40 +0100

    Testing functions for equality

--- test.dart
+++ test.dart
@@ -7,14 +7,22 @@
 // }
 
 void main() {
+  var add2 = makeAdder(2);
+  var add4 = makeAdder(4);
+  assert(add2(3) == 5);
+  assert(add4(3) == 7);
   const list = [
     'apples',
     'banana',
     'oranges',
     'yam',
     'pawpawapple'
-  ]; // defining an anonymous function with a typed parameter - item and passes it to map function. 
+  ]; // defining an anonymous function with a typed parameter - item and passes it to map function.
   list.map((item) => item.toUpperCase()).forEach((item) {
     print('$item: ${item.length}');
   });
+}
+
+Function makeAdder(int addBy) {
+  return (int i) => addBy + 1;
 }
---
+++ test1.dart
@@ -0,0 +1,22 @@
+// testing functions for equality.
+
+void foo() {}
+
+class A {
+  static void bar() {}
+  void baz() {}
+}
+
+void main() {
+  Function x;
+  x = foo;
+  assert(foo == x);
+  x = A.bar;
+  assert(A.bar == x);
+  var v = A();
+  var w = A();
+  var y = w;
+  x = w.baz;
+  assert(y.baz == x);
+  assert(v.baz != w.baz);
+}
