b5896b7bf4967dbceacb53976e4af9e79f5bc980
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-05-22 20:52:22 +0100

    Update language_tour.dart

--- language_tour.dart
+++ language_tour.dart
@@ -1,3 +1,14 @@
+// Introduction to Dart - Every app needs the top-level main() function, where execution starts.
 
-// Introduction to Dart
+void main() {
+  print('Hello, World');
+}
 
+// Variables - most variables can be specified using var. 
+/// var name = 'voyag';
+/// var year = 1922; 
+/// var flybyObjects = ['Jupiter', 'Saturn']
+/// var image = {'tags: [saturn]}
+/// 
+// Control Flow statement: 
+/// using the if, else-if, for, while
